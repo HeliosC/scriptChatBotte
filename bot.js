2a61467bfe8a335961fd28ec588190646cb4f68a
HeliosC <36132864+HeliosC@users.noreply.github.com>
2019-04-17 01:26:05 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,76 @@
+let date = new Date();
+console.log(date.getHours())
+
+var fs = require('fs');
+
+
+const redisToken = "redis://h:pa2565d85984f854fdb083158727b752ad192735778ef60f16a1b76f5f0373304@ec2-52-211-79-208.eu-west-1.compute.amazonaws.com:16579"
+var redis = require('redis').createClient(redisToken);
+redis.on('connect', function () {
+    console.log('connected');
+});
+
+var contents = ""
+var content = ""
+
+for (var jour = 1; jour < 32; jour++) {
+
+    if(jour<10){
+        jour='0'+jour
+    }
+
+    let dat = jour+'/04/2019'
+    // console.log(dat.substr(6,4))
+    // console.log(dat.substr(3,2))
+    // console.log(dat.substr(0,2))
+    let path = 'chat/' + dat.substr(6,4)+ '/' + dat.substr(3,2) + '/' + dat.substr(0,2) + '-' + dat.substr(3,2) + '-' + dat.substr(6,4) + '.txt'
+    // console.log(dat.substr(6,9))
+
+    try {
+        // fs.statSync('path/to/file');
+        console.log('file or directory exists');
+        content = fs.readFileSync(path, 'utf8');
+        contents += content +'\n\n\n\n'
+        +'******************************************************************************************************************************************************************************************************************************************\n'
+        +'******************************************************************************************************************************************************************************************************************************************\n'
+        +'******************************************************************************************************************************************************************************************************************************************\n'
+        +'******************************************************************************************************************************************************************************************************************************************\n'
+        +'******************************************************************************************************************************************************************************************************************************************\n'
+    }
+    catch (err) {
+      if (err.code === 'ENOENT') {
+        console.log('file or directory does not exist');
+      }
+    }
+
+
+
+
+    try {
+        let chatredis = "chat/" + dat
+        redis.exists(chatredis, function (err, reply) {
+            if (reply === 1) {
+                // console.log('exists');
+                fs.appendFile(path, '', function (err) {
+                    redis.get(chatredis, function (err, reply) {
+                        fs.writeFileSync(path, reply, { mode: 0o755 });
+                    });
+                    if (err) throw err;
+                // console.log('File is created successfully.');
+            }); 
+
+            } else {
+                // console.log(chatredis + " existe pas")
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+let dat = '00/04/2019'
+let path = 'chat/' + dat.substr(6,4)+ '/' + dat.substr(3,2) + '/' + dat.substr(0,2) + '-' + dat.substr(3,2) + '-' + dat.substr(6,4) + '.txt'
+//fs.writeFileSync(path, contents, { mode: 0o755 });
+
+console.log("cccccccccccccccccccccccccccccccccc")
