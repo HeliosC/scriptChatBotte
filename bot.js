16faea636f791f9bae188cf34bf2557eb51792f5
HeliosProd <magicchris11@live.fr>
2019-04-20 02:30:31 +0200

    no message

--- bot.js
+++ bot.js
@@ -2,13 +2,13 @@ mode = "word"
 let user = "pouidesbois"
 let word = "chatdehi"
 
-let day0 = 'process.env.dateBeginDay'
-let month0 = 'process.env.dateBeginMonth'
-let year0 = 'process.env.dateBeginYear'
+let day0 = process.env.dateBeginDay
+let month0 = process.env.dateBeginMonth
+let year0 = process.env.dateBeginYear
 
-let day1 = 'process.env.dateEndDay'
-let month1 = 'process.env.dateEndMonth'
-let year1 = 'process.env.dateEndYear'
+let day1 = process.env.dateEndDay
+let month1 = process.env.dateEndMonth
+let year1 = process.env.dateEndYear
 
 day0int = parseInt(day0)
 month0int = parseInt(month0)
@@ -35,9 +35,9 @@ if (mode == "word") {
     regex = new RegExp("[0-9]{2}:[0-9]{2} \\[" + user + "\\] : .*\\n|(\\n|^)\\*{32} Chat du [0-9]{2}\\/[0-9]{2}\\/[0-9]{4} \\*{32}\\n", "gmi");
 }
 
-logrec(day0int)
+logrec()
 
-function logrec(day0int) {
+function logrec() {
 
     if (day0int < 10) {
         jour = '0' + day0int
@@ -89,17 +89,17 @@ function logrec(day0int) {
 function redisquit() {
     day0int += 1
     if (day0int < day1int + 1) {
-        logrec(day0int)
+        logrec()
     } else {
         month0int += 1
         day0int = 1
         if (month0int < month1int + 1) {
-            logrec(day0int)
+            logrec()
         } else {
             year0int += 1
             month0int = 1
             if (year0int < year1int + 1) {
-                logrec(day0int)
+                logrec()
             } else {
                 var tag = day0 + "/" + month0 + "/" + year0 + "-" + day1 + "/" + month1 + "/" + year1
                 console.log('stop');
